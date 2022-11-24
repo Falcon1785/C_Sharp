d0edb1c4c48dcc739cdfcbfa43e8c6821ec3cff5
Falcon <soviol@mail.ru>
2022-11-24 08:18:13 +0300

    Task 1.2

---
+++ 1_Lesson/1.2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая на вход
+// принимает два числа и проверяет, является ли первое
+// число квадратом второго.
+
+Console.WriteLine("Write a number:");
+
+int num = int.Parse(Console.ReadLine());
+
+if(num == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if(num == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+else if(num == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+else if(num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if(num == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+else if(num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+else if(num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+else
+    Console.WriteLine("Ошибка");
