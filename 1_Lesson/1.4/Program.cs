af10f34616c7468ceb18e460d99d311071fe0321
Falcon <soviol@mail.ru>
2022-11-24 09:56:24 +0300

    Task 1.4

---
+++ 1_Lesson/1.4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Console.WriteLine("ВВедите число: ");
+// var s = Console.ReadLine();
+// int N = s == null ? 0 : int.Parse(s);
+// Console.Writeline($"{Math.Abs(N)}"); 
+
+
+Console.WriteLine("ВВедите число: ");
+int x = int.Parse(Console.ReadLine());
+// string a = Console.ReadLine();
+// int x = int.Parse(a);
+
+if(x < 0)
+{
+    Console.WriteLine(-x);
+}
+ else
+    Console.WriteLine(x);
