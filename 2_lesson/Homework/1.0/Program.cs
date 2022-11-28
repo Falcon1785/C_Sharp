7fa70eaa5003a90c308ddc43d7be7d6c351d04dd
Falcon <soviol@mail.ru>
2022-11-28 18:17:48 +0300

    Homework Task 1.0

--- 2_lesson/Homework/1.0/Program.cs
+++ 2_lesson/Homework/1.0/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую цифру этого
+//числа.
+
+//int num = int.Parse(Console.ReadLine());
+//Console.WriteLine((num / 10) % 10);
+
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+//Console.WriteLine((num / 10) % 10);
+
+int MiddleD(int num)
+{
+    return ((num / 10) % 10);
+}
+
+Console.WriteLine(MiddleD(int.Parse(Console.ReadLine())));
