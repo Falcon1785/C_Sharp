00f5f36722dee614c1f177003f16cd0153ca4fde
Falcon <soviol@mail.ru>
2022-11-28 16:37:12 +0300

    Task 2.2

--- 2_lesson/2.1/Program.cs
+++ 2_lesson/2.1/Program.cs
@@ -2,15 +2,14 @@
 // трехзначное число и на выходе показывает
 // последнюю цифру этого числа.
 
-int num = new Random().Next(100, 1000);
+int Num(int num)
+{
+    return num % 10;
+}
+ Console.WriteLine(Num(new Random().Next(100, 1000)));
 
-Console.WriteLine(num);
+//int num = new Random().Next(100, 1000);
 
-Console.WriteLine(num % 10);
+//Console.WriteLine(num);
 
-//int Num(int num)
-//{
-    // return num % 10;
-//}
-// Console.WriteLine(Num(new Random().Next(100, 1000)));
-
+//Console.WriteLine(num % 10);
---
+++ 2_lesson/2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая будет принимать на
+//вход два числа и выводить, является ли второе число
+//кратным первому. Если число 2 не кратно числу 1, то
+//программа выводит остаток от деления.
+
+//int num1 = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+
+//if(num1 % num2 == 0)
+//{
+  //  Console.WriteLine("Кратно");
+//}
+//else
+  //  Console.WriteLine($"Не кратно, остаток - {num1 % num2}");
+
+string Mult(int num1, int num2)
+{
+if(num1 % num2 == 0)
+     return "Кратно";
+else
+    return $"некратно, остаток = {num1 % num2}";
+}
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Mult(num1, num2));
+
