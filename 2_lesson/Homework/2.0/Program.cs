f168a67b17a7b540e6d646ae046f53027379b996
Falcon <soviol@mail.ru>
2022-11-28 18:38:47 +0300

    Homework Task 2.0

--- 2_lesson/Homework/2.0/Program.cs
+++ 2_lesson/Homework/2.0/Program.cs
@@ -1,6 +1,14 @@
 ﻿//Напишите программу, которая выводит случайное
 //трёхзначное число и удаляет вторую цифру этого числа.
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
-Console.WriteLine((num / 100) * 10 + num % 10);
+//int num = new Random().Next(100, 1000);
+//Console.WriteLine(num);
+//Console.WriteLine((num / 100) * 10 + num % 10);
+
+int DelMiddle(int num)
+{
+    return (num / 100) * 10 + num % 10;
+}
+int numnew = new Random().Next(100, 1000);
+Console.WriteLine(numnew);
+Console.WriteLine(DelMiddle(numnew));
