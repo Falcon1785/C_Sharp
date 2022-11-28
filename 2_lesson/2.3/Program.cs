fe87c45bbd3f87aba9a8079f992c084ec4843c5c
Falcon <soviol@mail.ru>
2022-11-28 17:19:21 +0300

    Task 2.4

--- 2_lesson/2.3/Program.cs
+++ 2_lesson/2.3/Program.cs
@@ -2,3 +2,15 @@
 //число и проверяет, кратно ли оно одновременно
 //7 и 23.
 
+string Mult(int num)
+{
+if(num % 7 == 0)
+    if(num % 23 == 0)
+        return "Да";
+    else 
+        return "Нет";
+else
+    return "Нет";         
+}
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Mult(num));
