d9e2c74b3955dc3181aa31b876ad0ba625df69c0
Falcon <soviol@mail.ru>
2022-12-04 03:40:48 +0300

    Correction Homework 3.0

--- 3_lesson/Homework/3.0/Program.cs
+++ 3_lesson/Homework/3.0/Program.cs
@@ -4,7 +4,7 @@
 
 void Pal(int num)
 {
-    if (num > 9999 || num < 99999 && num > -99999 || num < -9999)
+    if (num > 9999 && num < 99999 || num > -99999 && num < -9999)
     if (num / 10000 == num % 10 && (num / 1000) % 10 == (num % 100) / 10)
         Console.WriteLine($"{num} -> да");
     else
