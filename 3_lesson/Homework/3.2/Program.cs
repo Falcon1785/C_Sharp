cc35134489decaded1da02ba3f1450ab224d7c4e
Falcon <soviol@mail.ru>
2022-12-04 03:02:10 +0300

    Homework 3.1 & 3.2

---
+++ 3_lesson/Homework/3.2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void List(int N)
+{
+    int i = 1;
+    if (N > 0)
+
+        while (i <= N)
+        {
+            if (i == N)
+            {
+                Console.WriteLine($"{Math.Pow(i, 3)} ");
+            }
+            else
+            {
+                Console.Write($"{Math.Pow(i, 3)}, ");
+            }
+            i++;
+        }
+    else
+        while (i >= N)
+        {
+            if (i == N)
+            {
+                Console.WriteLine($"{Math.Pow(i, 3)} ");
+            }
+            else
+            {
+                Console.Write($"{Math.Pow(i, 3)}, ");
+            }
+            i--;
+        }
+}
+Console.WriteLine("Enter a number");
+int N = int.Parse(Console.ReadLine());
+List(N);
