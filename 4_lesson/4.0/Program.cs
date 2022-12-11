0b8445e7171775bf30230ebdd8ee32bb68cab16c
Falcon <soviol@mail.ru>
2022-12-11 05:36:27 +0300

    Task 4.1

--- 4_lesson/4.0/Program.cs
+++ 4_lesson/4.0/Program.cs
@@ -3,28 +3,28 @@
 
 //int Sum(int A)
 //{
-   // int i = 1;
-    //int S = 0;
-   // while (i <= A)
-   // {
-   //     S = S + i;
-  //      i++;
-  //  }
-  //  return S;
+// int i = 1;
+//int S = 0;
+// while (i <= A)
+// {
+//     S = S + i;
+//      i++;
+//  }
+//  return S;
 //}
 
 //Console.WriteLine(Sum(int.Parse(Console.ReadLine())));
 
 int SumNums(int num)
-	{
-	    int all_sum = 0;
-	    for (int i = 1; i <= num; i++)
-	    {
-	        all_sum += i;
-	    }
-	
-	    return all_sum;
-	}
-	
-	Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += i;
+    }
+
+    return all_sum;
+}
+
+Console.WriteLine(SumNums(int.Parse(Console.ReadLine())));
 
