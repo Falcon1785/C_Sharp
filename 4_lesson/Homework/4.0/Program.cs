12c5253c8e15a5b286baa16c343915dd81a6c5e2
Falcon <soviol@mail.ru>
2022-12-11 20:35:25 +0300

    Homework

---
+++ 4_lesson/Homework/4.0/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход
+//два числа (A и B) и возводит число A в натуральную степень B.
+
+int Expo(int A, int B)
+{
+    int all_mult = A;
+    for (int count = 1; count < B; count++)
+    {
+        all_mult = all_mult * A;
+    }
+    return all_mult;
+}
+
+Console.WriteLine("enter A:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("enter B:");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine(Expo(A, B));
+
+
