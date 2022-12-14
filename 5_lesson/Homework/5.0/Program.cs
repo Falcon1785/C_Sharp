cb00417e5a6a7b2ccc53f3b02c60b35856882544
Falcon <soviol@mail.ru>
2022-12-14 20:39:08 +0300

    Homework 5

---
+++ 5_lesson/Homework/5.0/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+int[] FillArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(from, to);
+    return arr;
+}
+
+int IntCount(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+
+        if (arr[i] >= 0)
+            count++;
+            
+    return count;
+    Console.WriteLine();
+}
+
+int[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine(IntCount(arr_1));
---
+++ 5_lesson/Homework/5.2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
+
+void Print(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+double[] FillArray(int size, int from, int to)
+{
+    double[] arr = new double[size];
+
+    for (int i = 0; i < size; i++)
+        arr[i] = Math.Round((new Random().NextDouble() * (to - from) + from), 2);
+    return arr;
+}
+
+void MaxMinDiff(double[] arr)
+{
+    double max = arr[0];
+    double min = arr[0];
+    for (int i = 0; i + 1 < arr.Length; i++)
+    {
+        if (max < arr[i + 1]) max = arr[i + 1];
+        if (min > arr[i + 1]) min = arr[i + 1];
+    }
+    Console.WriteLine($"{max} - ({min}) = {max - min}");
+}
+
+double[] arr_1 = FillArray(int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+MaxMinDiff(arr_1);
