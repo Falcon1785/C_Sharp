12d350e91a4d8b114c179ec6237edbd6e2759eb3
Falcon <soviol@mail.ru>
2022-12-15 07:21:40 +0300

    6_lesson Tasks

--- 5_lesson/5.4/Program.cs
+++ 5_lesson/5.4/Program.cs
@@ -46,39 +46,39 @@ Print(Mult(arr_1));
 
 //=========== Вариант 2 ===========
 
-void Print(int[] arr)
-	{
-	    int size = arr.Length;
+// void Print(int[] arr)
+// 	{
+// 	    int size = arr.Length;
 	
-	    for (int i = 0; i < size; i++)
-	        Console.Write($"{arr[i]} ");
-	    Console.WriteLine();
-	}
+// 	    for (int i = 0; i < size; i++)
+// 	        Console.Write($"{arr[i]} ");
+// 	    Console.WriteLine();
+// 	}
 	
-	int[] MassNums(int size)
-	{
-	    int[] arr = new int[size];
+// 	int[] MassNums(int size)
+// 	{
+// 	    int[] arr = new int[size];
 	
-	    for (int i = 0; i < size; i++)
-	        arr[i] = new Random().Next(1, 11);
-	    return arr;
-	}
+// 	    for (int i = 0; i < size; i++)
+// 	        arr[i] = new Random().Next(1, 11);
+// 	    return arr;
+// 	}
 	
-	int[] PairsNum(int[] arr)
-	{
-	    int size = arr.Length;
-	    int flex_size = size / 2 + size % 2;
-	    int[] new_arr = new int[flex_size];
+// 	int[] PairsNum(int[] arr)
+// 	{
+// 	    int size = arr.Length;
+// 	    int flex_size = size / 2 + size % 2;
+// 	    int[] new_arr = new int[flex_size];
 	
-	    for (int i = 0; i < size / 2; i++)
-	        new_arr[i] = arr[i] * arr[size - i - 1];
+// 	    for (int i = 0; i < size / 2; i++)
+// 	        new_arr[i] = arr[i] * arr[size - i - 1];
 	
-	    if (new_arr[flex_size - 1] == 0)
-	        new_arr[flex_size - 1] = arr[flex_size - 1];
-	    return new_arr;
-	}
+// 	    if (new_arr[flex_size - 1] == 0)
+// 	        new_arr[flex_size - 1] = arr[flex_size - 1];
+// 	    return new_arr;
+// 	}
 	
-	int[] arr_1 = MassNums(5);
-	Print(arr_1);
-	int[] arr_1_new = PairsNum(arr_1);
-	Print(arr_1_new);
+// 	int[] arr_1 = MassNums(5);
+// 	Print(arr_1);
+// 	int[] arr_1_new = PairsNum(arr_1);
+// 	Print(arr_1_new);
