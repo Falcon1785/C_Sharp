f2a4539d7068b0512bed845f1b68e1a021d3d3b3
Falcon <soviol@mail.ru>
2022-12-22 17:25:31 +0300

    Lesson 7

---
+++ 7_lesson/7.2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых
+// обе позиции чётные, и замените эти элементы на их квадраты.
+
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+
+            Console.Write($"{arr[i, j],4} ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void Degree(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 1; i < row; i += 2)
+         for (int j = 1; j < column; j += 2)
+              arr[i, j] *= arr[i, j];
+}
+
+Console.WriteLine("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+Console.WriteLine("from: ");
+int from = int.Parse(Console.ReadLine());
+Console.WriteLine("to: ");
+int to = int.Parse(Console.ReadLine());
+int[,] arr_1 = FillArray(row, column, from, to);
+Print(arr_1);
+Degree(arr_1);
+Print(arr_1);
