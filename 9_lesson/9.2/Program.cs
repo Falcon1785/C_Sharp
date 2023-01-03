772327af3d9674ae7ed30e25499b5fb9ac74a066
Falcon <soviol@mail.ru>
2023-01-04 00:00:08 +0300

    Tasks 9.1 - 9.4

---
+++ 9_lesson/9.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void NatNumBtw(int M, int N)
+{
+    if (M > N) return;
+    Console.Write($"{M} ");
+    NatNumBtw(M + 1, N);
+}
+NatNumBtw(4, 8);
+Console.WriteLine();
